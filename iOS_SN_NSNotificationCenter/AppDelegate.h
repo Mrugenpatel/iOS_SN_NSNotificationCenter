5655fc54ae47b6fdfc9b39c49bfee151c1faec39
naner121 <17090407852zzq@gmail.com>
2015-12-03 23:44:12 +0800

    Initial Commit

---
+++ iOS_SN_NSNotificationCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_SN_NSNotificationCenter
+//
+//  Created by mac on 15/12/3.
+//  Copyright © 2015年 kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_SN_NSNotificationCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_SN_NSNotificationCenter
+//
+//  Created by mac on 15/12/3.
+//  Copyright © 2015年 kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
